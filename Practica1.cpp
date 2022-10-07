4eddf08ac80f5b0d48ddbab4bdae257362a8e077
Irene <ireneneri001@gmail.com>
2022-10-07 16:40:59 +0200

    Create Practica1.cpp

---
+++ Practica1.cpp
@@ -0,0 +1,77 @@
+// ConsoleApplication2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+#include <GL/glut.h>
+
+using namespace std;
+
+// CONSTANTES
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const int WINDOW_POSITION_X = 300;
+const int WINDOW_POSITION_Y = 400;
+const char TITLE[] = "PARTE1";
+
+const GLclampf RED = 0.2;
+const GLclampf GREEN = 0.6;
+const GLclampf BLUE = 0.5;
+const GLclampf ALPHA = 1.0;
+
+// FUNCION DE ESCRIBIR
+void writeLine(string cadena) {
+    cout << cadena << endl;
+}
+
+// FUNCIONES OPENGL 
+
+void initGraphics() {
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_COLOR_MATERIAL);
+    glClearColor(RED, GREEN, BLUE, ALPHA);
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+    writeLine ("Hello World!");
+
+    writeLine("Generando ventana...");
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutInitWindowPosition(WINDOW_POSITION_X, WINDOW_POSITION_Y);
+    glutCreateWindow("title");
+
+    writeLine("Registrando funciones de tratamiento de eventos...");
+    glutDisplayFunc(display);
+
+    writeLine("Iniciando graficos...");
+    initGraphics();
+
+    writeLine("Iniciando bucle infinito...");
+    glutMainLoop();
+
+    writeLine("Iniciando bucle infinito");
+    glutMainLoop();
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
